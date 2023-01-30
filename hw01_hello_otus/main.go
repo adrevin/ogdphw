17051270a033befa9ab41e93c785efa573d6d40e
Andrey V Drevin <andrey@drev.in>
2023-01-30 14:36:59 +0300

    fixed tests

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
+
 	"golang.org/x/example/stringutil"
 )
 
-const hello = "Hello, OTUS!"
-
 func main() {
-	fmt.Println(stringutil.Reverse(hello))
+	fmt.Println(stringutil.Reverse("Hello, OTUS!"))
 }
