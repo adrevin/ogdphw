6e28bf90a08b333e87f201e77c3cee9961a29d39
Andrey V Drevin <andrey@drev.in>
2023-02-07 20:20:27 +0300

    removed strings.Repeat(...

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,19 +9,26 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(inp string) (string, error) {
 	var builder strings.Builder
-	var char string
+	var char *rune = nil
 	for _, c := range inp {
 		if c > 47 && c < 58 { // it is a digit
-			if char == "" { // it is no previous non digit char
+			if char == nil { // it is no previous non digit char
 				return "", ErrInvalidString
 			}
-			builder.WriteString(strings.Repeat(char, int(c-48))) // repeat previous char
-			char = ""                                            // reset repeated char
+			for t := 0; t < int(c-48); t++ { // repeat previous char
+				builder.WriteRune(*char)
+			}
+			char = nil // reset repeated char
 		} else {
-			builder.WriteString(char) // it is previous non digit char. just use it to output
-			char = string(c)          // remember char as previous
+			if char != nil {
+				builder.WriteRune(*char) // it is previous non digit char. just use it to output
+			}
+			r := c
+			char = &r // remember char as previous
 		}
 	}
-	builder.WriteString(char) // process last char
+	if char != nil {
+		builder.WriteRune(*char) // it is previous non digit char. just use it to output
+	}
 	return builder.String(), nil
 }
