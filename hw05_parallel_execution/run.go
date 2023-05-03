74ff2a5209b963a5e396e65630974c0c11e316f9
Andrey V Drevin <andrey@drev.in>
2023-05-03 10:14:38 +0300

    chan's logic applied

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -11,12 +11,10 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 type handler struct {
-	taskIndex       int32
-	maxTaskIndex    int32
 	errorsNumber    int32
 	wg              sync.WaitGroup
 	maxErrorsNumber int32
-	tasks           *[]Task
+	tasks           chan Task
 }
 
 func Run(tasks []Task, n, m int) error {
@@ -25,12 +23,14 @@ func Run(tasks []Task, n, m int) error {
 	}
 
 	h := &handler{
-		taskIndex:       -1,
-		maxTaskIndex:    int32(len(tasks) - 1),
-		errorsNumber:    0,
 		maxErrorsNumber: int32(m),
-		tasks:           &tasks,
+		tasks:           make(chan Task, len(tasks)),
 	}
+
+	for i := 0; i < len(tasks); i++ {
+		h.tasks <- tasks[i]
+	}
+	close(h.tasks)
 
 	for i := 0; i < n; i++ {
 		h.wg.Add(1)
@@ -38,7 +38,6 @@ func Run(tasks []Task, n, m int) error {
 	}
 
 	h.wg.Wait()
-
 	// Значение m <= 0 трактуется как знак игнорировать ошибки в принципе
 	if m <= 0 {
 		return nil
@@ -52,15 +51,11 @@ func Run(tasks []Task, n, m int) error {
 
 func work(h *handler) {
 	defer h.wg.Done()
-	for {
-		index := atomic.AddInt32(&h.taskIndex, 1)
-		if index > h.maxTaskIndex {
-			return
-		}
+	for task := range h.tasks {
 		if h.maxErrorsNumber > 0 && atomic.LoadInt32(&h.errorsNumber) >= h.maxErrorsNumber {
 			return
 		}
-		if (*h.tasks)[index]() != nil {
+		if task() != nil {
 			atomic.AddInt32(&h.errorsNumber, 1)
 		}
 	}
