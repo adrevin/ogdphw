6c514d1cac94c50748991d90bc8ef33c59329e8c
Andrey V Drevin <andrey@drev.in>
2023-05-03 18:33:23 +0300

    wip

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,61 +2,14 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"sync"
-	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-type handler struct {
-	errorsNumber    int32
-	wg              sync.WaitGroup
-	maxErrorsNumber int32
-	tasks           chan Task
-}
-
+// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	if n <= 0 {
-		panic("n must be greater than 0")
-	}
-
-	h := &handler{
-		maxErrorsNumber: int32(m),
-		tasks:           make(chan Task, len(tasks)),
-	}
-
-	for i := 0; i < len(tasks); i++ {
-		h.tasks <- tasks[i]
-	}
-	close(h.tasks)
-
-	for i := 0; i < n; i++ {
-		h.wg.Add(1)
-		go work(h)
-	}
-
-	h.wg.Wait()
-	// Значение m <= 0 трактуется как знак игнорировать ошибки в принципе
-	if m <= 0 {
-		return nil
-	}
-
-	if h.errorsNumber >= h.maxErrorsNumber {
-		return ErrErrorsLimitExceeded
-	}
+	// Place your code here.
 	return nil
-}
-
-func work(h *handler) {
-	defer h.wg.Done()
-	for task := range h.tasks {
-		if h.maxErrorsNumber > 0 && atomic.LoadInt32(&h.errorsNumber) >= h.maxErrorsNumber {
-			return
-		}
-		if task() != nil {
-			atomic.AddInt32(&h.errorsNumber, 1)
-		}
-	}
 }
