3120c87cc2739a7dbe587f845114193e9b1186da
Andrey V Drevin <andrey@drev.in>
2023-05-04 16:16:39 +0300

    changes by review

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,39 +8,30 @@ type (
 
 type Stage func(in In) (out Out)
 
-type handler struct {
-	out Out
-}
-
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	if in == nil {
-		panic("in is nil")
-	}
 	if stages == nil {
 		panic("stages is nil")
 	}
-	h := &handler{out: chanProxy(in, done)}
+	out := chanProxy(in, done)
 	for _, stage := range stages {
-		h.out = chanProxy(stage(h.out), done)
+		out = chanProxy(stage(out), done)
 	}
-	return h.out
+	return out
 }
 
 func chanProxy(in In, done In) Out {
 	out := make(Bi)
 	go func() {
+		defer close(out)
 		for {
 			select {
 			case <-done:
-				close(out)
 				return
 			case v, ok := <-in:
-				if ok {
-					out <- v
-				} else {
-					close(out)
+				if !ok {
 					return
 				}
+				out <- v
 			}
 		}
 	}()
