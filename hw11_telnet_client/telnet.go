dcb04949f95aed94afb7a69bff05e7bb0b229de4
Andrey V Drevin <andrey@drev.in>
2023-05-12 12:53:47 +0300

    hw11

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
@@ -11,10 +12,51 @@ type TelnetClient interface {
 	Send() error
 	Receive() error
 }
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{address: address, timeout: timeout, in: in, out: out}
+}
+
+func (client *telnetClient) Connect() error {
+	conn, err := net.Dial("tcp", client.address)
+	if err != nil {
+		return err
+	}
+	client.conn = conn
 	return nil
+}
+
+func (client *telnetClient) Send() error {
+	buf, err := io.ReadAll(client.in)
+	if err != nil {
+		return err
+	}
+	_, err = client.conn.Write(buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (client *telnetClient) Receive() error {
+	buf, err := io.ReadAll(client.conn)
+	if err != nil {
+		return err
+	}
+	_, err = client.out.Write(buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (client *telnetClient) Close() error {
+	return client.conn.Close()
 }
 
 // Place your code here.
