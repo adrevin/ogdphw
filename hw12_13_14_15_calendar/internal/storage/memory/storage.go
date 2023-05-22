4154243cc4e5f705ecba311bededa03abad5f594
Andrey V Drevin <andrey@drev.in>
2023-05-23 00:26:56 +0300

    wip

--- hw12_13_14_15_calendar/internal/storage/memory/storage.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage.go
@@ -1,48 +1,127 @@
 package memorystorage
 
 import (
+	"errors"
 	"sync"
 	"time"
 
 	"github.com/adrevin/ogdphw/hw12_13_14_15_calendar/internal/entities"
 	"github.com/google/uuid"
+	"github.com/snabb/isoweek"
 )
 
 type Storage struct {
-	mu sync.RWMutex //nolint:unused
+	mu     *sync.RWMutex
+	events map[uuid.UUID]*entities.Event
+	days   map[time.Time]map[uuid.UUID]*entities.Event
+	weeks  map[time.Time]map[uuid.UUID]*entities.Event
+	months map[time.Time]map[uuid.UUID]*entities.Event
 }
 
 func New() *Storage {
-	return &Storage{}
+	return &Storage{
+		events: make(map[uuid.UUID]*entities.Event),
+		days:   make(map[time.Time]map[uuid.UUID]*entities.Event),
+		weeks:  make(map[time.Time]map[uuid.UUID]*entities.Event),
+		months: make(map[time.Time]map[uuid.UUID]*entities.Event),
+		mu:     &sync.RWMutex{},
+	}
 }
 
-func (l Storage) Create(event entities.Event) (uuid.UUID, error) { //nolint:govet
+var ErrEventNotFound = errors.New("unsupported file")
+
+func (l Storage) Create(event *entities.Event) uuid.UUID {
+	defer l.mu.RUnlock()
+
+	l.mu.RLock()
 	event.ID = uuid.New()
-	// TODO
-	return event.ID, nil
+	l.save(event)
+
+	return event.ID
 }
 
-func (l Storage) Update(id uuid.UUID, event entities.Event) error { //nolint:govet,revive
-	// TODO
+func (l Storage) Update(id uuid.UUID, event *entities.Event) error {
+	defer l.mu.RUnlock()
+
+	l.mu.RLock()
+	if l.events[id] == nil {
+		return ErrEventNotFound
+	}
+	event.ID = id
+	l.save(event)
 	return nil
 }
 
-func (l Storage) Delete(id uuid.UUID) error { //nolint:govet,revive
-	// TODO
+func (l Storage) Delete(id uuid.UUID) error {
+	defer l.mu.RUnlock()
+
+	l.mu.RLock()
+	event := l.events[id]
+	if l.events[id] == nil {
+		return ErrEventNotFound
+	}
+
+	delete(l.events, event.ID)
+
+	dayKey := dayKey(event.Time)
+	delete(l.days[dayKey], event.ID)
+
+	monthKey := monthKey(event.Time)
+	delete(l.months[monthKey], event.ID)
+
+	weekKey := weekKey(event.Time)
+	delete(l.weeks[weekKey], event.ID)
+
 	return nil
 }
 
-func (l Storage) DayEvens(time time.Time) ([]entities.Event, error) { //nolint:govet,revive
+func (l Storage) DayEvens(time time.Time) ([]entities.Event, error) { //nolint:revive
 	// TODO
 	return make([]entities.Event, 0), nil
 }
 
-func (l Storage) WeekEvens(time time.Time) ([]entities.Event, error) { //nolint:govet,revive
+func (l Storage) WeekEvens(time time.Time) ([]entities.Event, error) { //nolint:revive
 	// TODO
 	return make([]entities.Event, 0), nil
 }
 
-func (l Storage) MonthEvens(time time.Time) ([]entities.Event, error) { //nolint:govet,revive
+func (l Storage) MonthEvens(time time.Time) ([]entities.Event, error) { //nolint:revive
 	// TODO
 	return make([]entities.Event, 0), nil
+}
+
+func (l Storage) save(event *entities.Event) {
+	l.events[event.ID] = event
+
+	dayKey := dayKey(event.Time)
+	if l.days[dayKey] == nil {
+		l.days[dayKey] = make(map[uuid.UUID]*entities.Event)
+	}
+	l.days[dayKey][event.ID] = event
+
+	weekKey := weekKey(event.Time)
+	if l.weeks[weekKey] == nil {
+		l.weeks[weekKey] = make(map[uuid.UUID]*entities.Event)
+	}
+	l.weeks[weekKey][event.ID] = event
+
+	monthKey := monthKey(event.Time)
+	if l.months[monthKey] == nil {
+		l.months[monthKey] = make(map[uuid.UUID]*entities.Event)
+	}
+}
+
+var location = time.UTC
+
+func dayKey(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, location)
+}
+
+func monthKey(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), 0, 0, 0, 0, 0, location)
+}
+
+func weekKey(t time.Time) time.Time {
+	year, week := isoweek.FromDate(t.Year(), t.Month(), t.Day())
+	return isoweek.StartTime(year, week, location)
 }
--- hw12_13_14_15_calendar/internal/storage/memory/storage_test.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage_test.go
@@ -10,17 +10,34 @@ import (
 )
 
 func TestStorage(t *testing.T) {
-	t.Run("basic", func(t *testing.T) {
-		evt := entities.Event{
-			ID:       uuid.Nil,
-			Title:    "Title",
-			Time:     time.Now().UTC(),
-			Duration: time.Hour,
-			UserID:   uuid.New(),
-		}
-		storage := &Storage{}
-		evtID, err := storage.Create(evt)
-		require.NoError(t, err)
+	storage := New()
+	evt := entities.Event{
+		Title:    "Title",
+		Time:     time.Now().UTC(),
+		Duration: time.Hour,
+		UserID:   uuid.New(),
+	}
+
+	t.Run("create", func(t *testing.T) {
+		evtID := storage.Create(&evt)
 		require.NotEqual(t, uuid.Nil, evtID)
+	})
+
+	t.Run("update", func(t *testing.T) {
+		err := storage.Update(uuid.New(), &entities.Event{})
+		require.ErrorIs(t, ErrEventNotFound, err)
+
+		evtID := storage.Create(&evt)
+		err = storage.Update(evtID, &entities.Event{Title: "NewTitle"})
+		require.NoError(t, err)
+	})
+
+	t.Run("delete", func(t *testing.T) {
+		err := storage.Delete(uuid.New())
+		require.ErrorIs(t, ErrEventNotFound, err)
+
+		evtID := storage.Create(&evt)
+		err = storage.Delete(evtID)
+		require.NoError(t, err)
 	})
 }
